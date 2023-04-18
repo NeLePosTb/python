0ea2a68d32f125d549c17f20f9fb75027bbb8a74
Vlad <vmereskin@gmail.com>
2023-04-19 00:11:17 +0300

    change task 2 and task 3

--- homework2/task3.py
+++ homework2/task3.py
@@ -1,9 +1,6 @@
-Up = 1
-def two_numbers(end):
-    for i in range(1,end):
-        if 2**Up == i :
-            Up += 1
-            print(i)
-            return i
-
-two_numbers(input("Введите число: "))
+n = int(input("Введите до скольки считать: "))
+power = 2
+print("Натуральные степени числа 2: ")
+while power <= n:
+    print(power)
+    power *= 2
