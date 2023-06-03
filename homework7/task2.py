125bec6707da401f1c91564b421404c9550f2575
Vlad <vmereskin@gmail.com>
2023-06-03 13:32:38 +0300

    add task 2 from homework7

---
+++ homework7/task2.py
@@ -0,0 +1,11 @@
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for i in range(1, num_rows+1):
+        for j in range(1, num_columns+1):
+            print(operation(i, j), end='\t')
+        print()
+
+def multiply(x, y):
+    return x * y
+
+print_operation_table(multiply)
